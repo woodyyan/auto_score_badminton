1512816510ff74cabb295067a2493f375e2079d6
Songbai Yan <colorguitar@hotmail.com>
2020-01-14 13:57:17 +0800

    add test test check_score

--- src/service_runner.py
+++ src/service_runner.py
@@ -1,26 +1,37 @@
+class ServiceRunner:
 
-def find_request_service_class():
-    pass
+    def find_request_service_class(self):
+        pass
 
-def run_request_service() -> str:
-    pass
+    def run_request_service(self) -> str:
+        pass
 
-def check_score(printed_message: str) -> int:
-    first_line = '********Price********'
-    second_line = 'Welcome to badminton'
-    last_line = '**Have a good day !**'
-    workday_part = '''-------Workday-------
+    def check_score(self, printed_message: str) -> int:
+        first_line = '********Price********'
+        second_line = 'Welcome to badminton'
+        last_line = '**Have a good day !**'
+        workday_part = '''-------Workday-------
 9:00~12:00 30 yuan/h
 12:00~18:00 50 yuan/h
 18:00~20:00 80 yuan/h
 20:00~22:00 60 yuan/h'''
-    weekend_part = '''-------Weekend-------
+        weekend_part = '''-------Weekend-------
 9:00~12:00 40 yuan/h
 12:00~18:00 50 yuan/h
 18:00~22:00 60 yuan/h'''
-
-    
+        lines = printed_message.split('\n')
+        score = 0
+        if first_line in printed_message and second_line in printed_message and last_line in printed_message:
+            score += 5
+        if workday_part in printed_message:
+            score += 15
+        if weekend_part in printed_message:
+            score += 10
+        if len(lines) == 12:
+            score += 10
+        return score
 
 
 if __name__ == '__main__':
-    find_request_service_class()
+    service_runner = ServiceRunner()
+
--- tests/test_service_runner.py
+++ tests/test_service_runner.py
@@ -1,9 +1,26 @@
 import unittest
 
+from src.service_runner import ServiceRunner
+
 
 class TestServiceRunnerCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_check_score(self):
+        printed_message = '''********Price********
+Welcome to badminton
+-------Workday-------
+9:00~12:00 30 yuan/h
+12:00~18:00 50 yuan/h
+18:00~20:00 80 yuan/h
+20:00~22:00 60 yuan/h
+-------Weekend-------
+9:00~12:00 40 yuan/h
+12:00~18:00 50 yuan/h
+18:00~22:00 60 yuan/h
+**Have a good day !**'''
+        service_runner = ServiceRunner()
+        score = service_runner.check_score(printed_message)
+
+        self.assertEqual(score, 40)
 
 
 if __name__ == '__main__':
