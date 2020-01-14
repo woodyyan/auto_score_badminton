f202dd6430b1eb7d0058c81edb44b7131da9ba6c
Songbai Yan <colorguitar@hotmail.com>
2020-01-14 12:32:47 +0800

    first commit

---
+++ src/service_runner.py
@@ -0,0 +1,26 @@
+
+def find_request_service_class():
+    pass
+
+def run_request_service() -> str:
+    pass
+
+def check_score(printed_message: str) -> int:
+    first_line = '********Price********'
+    second_line = 'Welcome to badminton'
+    last_line = '**Have a good day !**'
+    workday_part = '''-------Workday-------
+9:00~12:00 30 yuan/h
+12:00~18:00 50 yuan/h
+18:00~20:00 80 yuan/h
+20:00~22:00 60 yuan/h'''
+    weekend_part = '''-------Weekend-------
+9:00~12:00 40 yuan/h
+12:00~18:00 50 yuan/h
+18:00~22:00 60 yuan/h'''
+
+    
+
+
+if __name__ == '__main__':
+    find_request_service_class()
