7fd15e677127f59457924dbf508e1c741f8e6da5
Songbai Yan <colorguitar@hotmail.com>
2020-01-15 15:50:40 +0800

    fixed runner

--- src/badminton_examination.py
+++ src/badminton_examination.py
@@ -1,5 +1,5 @@
 from src.badminton_project_parser import BadmintonProjectParser
-from src.badminton_scorer import BadmintonScorer
+from src.badminton_scorer import BadmintonScorer, ScoreSummary
 
 
 class BadmintonExamination:
@@ -8,14 +8,14 @@ class BadmintonExamination:
         parser = BadmintonProjectParser()
         specs = parser.build_all_project_specs(dir)
         scorer = BadmintonScorer()
-        all_score_info = []
+        student_scores = []
         for spec in specs:
-            score = scorer.check(spec.badminton_service_dir)
-            all_score_info.append(ScoreInfo(spec.student_id, score))
-        return all_score_info
+            score_summary = scorer.check(spec.badminton_service_dir)
+            student_scores.append(StudentScore(spec.student_id, score_summary))
+        return student_scores
 
 
-class ScoreInfo:
-    def __init__(self, student_id, score):
+class StudentScore:
+    def __init__(self, student_id, score_summary: ScoreSummary):
         self.student_id = student_id
-        self.score = score
+        self.score_summary = score_summary
--- src/runner.py
+++ src/runner.py
@@ -1,9 +1,11 @@
-from src.badminton_scorer import BadmintonScorer
+from src.badminton_examination import BadmintonExamination
 
 if __name__ == '__main__':
-    badminton_scorer = BadmintonScorer()
-    specs = badminton_scorer.run('/Users/songbai.yan/Downloads/Python/auto')
-    print(specs[0].score)
-    print(specs[1].score)
-    print(specs[2].score)
-    print(specs[3].score)
+    examination = BadmintonExamination()
+    student_scores = examination.check('/Users/songbai.yan/Downloads/Python/auto')
+    print(len(student_scores))
+    for score in student_scores:
+        print(score.student_id)
+        print(score.score_summary.part_a_score)
+        print(score.score_summary.part_b_score)
+        print(score.score_summary.part_c_score)
